d577cb46f6cf9e7639ccfbfed2a5a03d96f316ec
willbritt <47224666+willbritt@users.noreply.github.com>
2019-02-21 01:14:12 -0500

    Update values.js

--- values.js
+++ values.js
@@ -2,7 +2,7 @@ var createList = function()
 {
  var names = [11,4,41,50,7,19,8,18,2,6,43,6,20,43,2,23,12,24,31,2];
  
- document.getElementById("mean").innerHTML = "19.5";
- document.getElementById("mode").innerHTML = "6,43";
- document.getElementById("median").innerHTML = "18.5";
+ document.getElementById("mean").innerHTML = "Mean: 19.5";
+ document.getElementById("mode").innerHTML = "Mode: 6,43";
+ document.getElementById("median").innerHTML = "Median: 18.5";
 }
